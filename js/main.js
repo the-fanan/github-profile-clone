d50577040c4d651c8cf0c4de9ca941d211462068
Fanan Dala <fanan.dala@yahoo.com>
2020-11-16 19:52:32 +0100

    feat(navbar): WIP add dropdown toggle

--- js/main.js
+++ js/main.js
@@ -12,6 +12,22 @@ function removeSearchFocus()
     searchBox.classList.remove("focused");
 }
 
+function toggleDropdown(elem)
+{   
+    for (let i = 0; i < elem.parentNode.childNodes.length; i++) {
+        let child = elem.parentNode.childNodes[i];
+        if (child.tagName !== undefined) {
+            if (child.classList.contains("dropdown")) {
+                if (child.style.display == "none" || child.style.display == "") {
+                    child.style.display = "inline-block"
+                } else {
+                    child.style.display = "none"
+                }
+            }
+        }
+    }
+}
+
 window.addEventListener('load', function () {
    
 })
