753cbed44f407bdbb46361f2069ac04b6f099a7e
Fanan Dala <fanan.dala@yahoo.com>
2020-11-18 20:25:53 +0100

    feat(api): add fetch user data

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function hideDropdown()
     for (let i = 0; i < dropdowns.length; i++) {
         let dropdown = dropdowns[i];
         if (dropdown.tagName !== undefined) {
-            dropdown.style.display = "none"
+            dropdown.style.display = "none";
         }
     }
 }
@@ -44,24 +44,75 @@ function toggleDropdown(event)
                 if (child.style.display == "none" || child.style.display == "") {
                     //close other dropdowns
                     hideDropdown()
-                    child.style.display = "inline-block"
+                    child.style.display = "inline-block";
                 } else {
-                    child.style.display = "none"
+                    child.style.display = "none";
                 }
             }
         }
     }
 }
 
+/**
+ *  Updates DOM with the fetched user data
+ * 
+ * @param {Object} data 
+ */
+function updateDOMWithProfileData(data)
+{
+
+}
+
+/**
+ * Gets the user data from Github GraphQL API
+ * 
+ * @return {Promise} 
+ */
+function getProfileData()
+{
+    let url = 'https://api.github.com/graphql';
+    let token = '7f2a3f84fa56e87eb66f5c2df1c3ea4ceeb4642f';
+    let headers = new Headers({
+        'Authorization': 'Bearer ' + token,
+        'Content-type': 'application/json',
+    })
+    let body = '{ "query": "query { user(login:\\\"the-fanan\\\") { name url login bio avatarUrl followers { totalCount } following { totalCount } location email twitterUsername websiteUrl starredRepositories { totalCount } status { id emoji } repositories(first: 20, orderBy: {field:NAME, direction:ASC}) { nodes { name url isPrivate forkCount stargazerCount primaryLanguage { id name color } owner { login } defaultBranchRef { name } } } } }" } ';
+
+    let result = new Promise((resolve, reject) => {
+        fetch(url, {
+            method: 'POST',
+            body,
+            headers,
+        })
+        .then((response) => response.json())
+        .then(function(data) {
+            resolve(data)
+        })
+        .catch(function(error){
+            reject(error);
+        })
+    })
+
+    return result;
+}
+
 window.addEventListener('load', function () {
     document.querySelectorAll('.dropdown-toggler').forEach(item => {
         item.addEventListener('click', event => {
-            toggleDropdown(event)
+            toggleDropdown(event);
         })
     })
     document.addEventListener('click', function(event) {
         if (!event.target.classList.contains("dropdown")) {
-            hideDropdown()
+            hideDropdown();
         }
+    })
+
+    getProfileData()
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.log(error)
     })
 })
